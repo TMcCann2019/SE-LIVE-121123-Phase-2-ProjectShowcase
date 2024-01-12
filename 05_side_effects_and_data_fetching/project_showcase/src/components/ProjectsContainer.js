52ae2513272cbdf7b28d48ff37937d7c161345fc
TimothyMcCann <timothysmccann@gmail.com>
2024-01-12 14:54:36 -0500

    second half of lecture

--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectsContainer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectsContainer.js
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 
 const ProjectsContainer = () => {
   const [projects, setProjects] = useState([]);
+  const [selectedPhase, setSelectedPhase] = useState("");
+  const [searchQuery, setSearchQuery] = useState("")
 
-  const loadProjects = () => {
-    fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projectsData) => setProjects(projectsData));
-  };
+  // const loadProjects = () => {
+  //   fetch("http://localhost:4000/projects")
+  //     .then((res) => res.json())
+  //     .then((projectsData) => setProjects(projectsData));
+  // };
+
+  useEffect(() => {
+    let url = "http://localhost:4000/projects"
+    if (selectedPhase && searchQuery) {
+      url += `?phase=${selectedPhase}&q=${searchQuery}`
+    } else if (selectedPhase){
+      url += `?phase=${selectedPhase}`
+    } else if (searchQuery) {
+      url += `?q=${searchQuery}`
+    }
+    fetch(url)
+      .then((res) => res.json()) //eventually would like to handle errors here
+      .then((projectsData) => setProjects(projectsData))
+  }, [selectedPhase, searchQuery]);
 
   const addProject = (newProject) => {
     console.log("🚀 ~ addProject ~ newProject:", newProject);
@@ -23,7 +39,13 @@ const ProjectsContainer = () => {
       <ProjectForm
         onAddProject={addProject} 
       />
-      <ProjectList onLoadProjects={loadProjects} projects={projects} />
+      <ProjectList 
+        projects={projects}
+        onSelectPhase={setSelectedPhase}
+        searchQuery={searchQuery}
+        onSearchChange={setSearchQuery}
+        selectedPhase={selectedPhase}
+       />
     </>
   );
 };
